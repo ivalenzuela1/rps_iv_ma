f4079b7e1c7c7471f447ed45871dc7316570ae88
Ignacio Valenzuela <ivalenzuela89@gmail.com>
2018-02-22 18:32:10 -0500

    rock_paper_scissors.js

---
+++ mario_blocks.js
@@ -0,0 +1,16 @@
+
+
+var height  = 5;
+var space = ' ';
+var pound = '#';
+
+for (var i=4; i < height; i--){
+
+	var spaceNum = space.repeat(i);
+	var poundNum = pound.repeat(i);
+
+	console.log(spaceNum + poundNum);
+
+}
+
+console.log("test");
--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,61 +1,47 @@
-var player1 = "Nacho";
-var player2 = "Marcel";
-var wins1 = 0;
-var wins2 = 0;
+var player1Name = 'Player 1';
+var player2Name = 'Player 2';
+var player1wins = 0;
+var player2wins = 0;
 var weapons = ['rock', 'paper', 'scissors'];
 
-// your code goes here
-while (wins1 < 3 && wins2 < 3){
+while(player1wins < 3 && player2wins <3) {
+
+  var weaponOfChoice1 =
+      weapons[Math.floor(Math.random() * weapons.length) % 3];
+  var weaponOfChoice2 =
+      weapons[Math.floor(Math.random() * weapons.length) % 3];
+  console.log('Player 1: ' + weaponOfChoice1);
+  console.log('Player 2: ' + weaponOfChoice2);
   
-  var weaponOfChoice1 = weapons[Math.floor(Math.random() * weapons.length) % 3];
-  var weaponOfChoice2 = weapons[Math.floor(Math.random() * weapons.length) % 3];
 
-  console.log(player1 + " Choice: " + weaponOfChoice1)
-  console.log(player2 + " Choice: " + weaponOfChoice2)
+  if (weaponOfChoice1 === weaponOfChoice2){
+    console.log("Its a Draw!");
 
-  if (weaponOfChoice1 === 'rock' && weaponOfChoice2 === 'scissors'){
-    console.log(player1 + " wins!");
-    wins1++;
-    console.log("wins1 " + wins1);
-    
-  }else if (weaponOfChoice1 === 'paper' && weaponOfChoice2 === 'rock'){
-    console.log(player1 + " wins!")
-    wins1++;
-    console.log("wins1 " + wins1);
-    
-  }else if (weaponOfChoice1 === 'scissors' && weaponOfChoice2 === 'paper'){
-    console.log(player1 + " wins!")
-    wins1++;
-    console.log("wins1 " + wins1);
+  } else if(weaponOfChoice1 === 'rock' && weaponOfChoice2 === 'scissors') {
+    player1wins++;
+    console.log('Player 1 wins');
 
-  } else if (weaponOfChoice2 === 'rock' && weaponOfChoice1 === 'scissors'){
-    console.log(player2 + " wins!");
-    wins2++;
-    console.log("wins2 " + wins2);
-    
-  }else if (weaponOfChoice2 === 'paper' && weaponOfChoice1 === 'rock'){
-    console.log(player2 + " wins!")
-    wins2++;
-    console.log("wins2 " + wins2);
-    
-  }else if (weaponOfChoice2 === 'scissors' && weaponOfChoice1 === 'paper'){
-    console.log(player2 + " wins!")
-    wins2++;
-    console.log("wins2 " + wins2);
+  } else if(weaponOfChoice1 === 'paper' && weaponOfChoice2 === 'rock') {
+    player1wins++;
+    console.log('Player 1 wins');
 
-   }
- }
+  } else if(weaponOfChoice1 === 'scissors' && weaponOfChoice2 === 'paper') {
+    player1wins++;
+    console.log('Player 1 wins');
 
+  } else {
+    player2wins++;
+    console.log('Player 2 wins');
 
+}
 
-    if (wins1 === 3){ 
-       console.log(player1 + " IS THE WINNER");
-      } else {console.log(player2 + " IS THE WINNER");}
+  console.log('Number of wins for Player 1: ' + player1wins);
+  console.log('Number of wins for Player 2: ' + player2wins);
+  console.log(' ');
+}
 
-
-
-
-
-
-
-
+if(player1wins === 3) {
+  console.log('Player 1 wins the game!');
+} else if (player2wins === 3) {
+  console.log('Player 2 wins the game!');
+}
